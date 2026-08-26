deeb220a8155e7497ed3b9e293cd208eaff14c7b
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-26 00:28:22 -0700

    dd

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@
 
     // 3. Inject Link into Footer
     if (footer && !document.querySelector(".eyecare")) {
-        footer.insertAdjacentHTML("beforeend", "<center><a style='user-select:none;text-decoration:underline;cursor:pointer;' title='Toggle goosettings' class='eyecare'>Open Goosettings</a></center>");
+        footer.insertAdjacentHTML("beforeend", "<center><ul><li><a style='user-select:none;text-decoration:underline;cursor:pointer;' title='Toggle goosettings' class='eyecare'>Open Goosettings</a></li><li><a href='https://thegoosesite.github.io'>Open New Site</a></li></ul></center>");
     }
 
     const state = {
