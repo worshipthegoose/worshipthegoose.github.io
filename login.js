ebf6ce12e09d0c8fb902a2390dd293d0dcd19e69
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-26 20:58:20 -0700

    Update login.js

--- login.js
+++ login.js
@@ -47,10 +47,10 @@ document.addEventListener("DOMContentLoaded", function() {
     if (igloo) {
       igloo.href = "/search";
       igloo.title = "Back to the search page...";
-      igloo.innerHTML = `The Goose Site Legacy<span style="font-weight:normal !important;font-size:13px;margin-left:5px;">Search</span>`;
+      igloo.innerHTML = `The Goose Site<span style="font-weight:normal !important;font-size:13px;margin-left:5px;">Search</span>`;
     }
   } else {
-    igloo.innerHTML = `The Goose Site <span style="font-weight:normal !important;font-size:13px;margin-left:5px;">Search</span>`;
+    igloo.innerHTML = `The Goose Site<span style="font-weight:normal !important;font-size:13px;margin-left:5px;">Search</span>`;
   }
   if (localStorage.getItem("fontGlobal") !== null){
     if (!document.querySelector("link[href*='global.css']")) {
